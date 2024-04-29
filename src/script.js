2e95b55825624d3bdb36300b6ca6b4d245a454f8
alemran22 <alemrangazi@gmail.com>
2024-04-29 21:00:12 +0600

    update

---
+++ src/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $('.container').slick({
+      // setting-name: setting-value
+      arrows: true,
+      prevArrow: ".backarrow",
+      nextArrow: ".nextarrow",
+       dots: true,
+//   infinite: true,
+//   speed: 300,
+//   slidesToShow: 4,
+//   slidesToScroll: 4,
+      fade: true,
+      
+  });
+});
+		
